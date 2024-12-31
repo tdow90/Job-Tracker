45b2925583f4b947873281047a254ea1a427932d
Tyler Downing <tyler.ar.downing@hotmail.com>
2024-12-31 17:38:06 -0400

    Added the scraper to a view for now, this is how we can scrape new jobs into the DB. Eventually, i'll update to a celery worker.

--- JobTracker/JobApi/job_scraper.py
+++ JobTracker/JobApi/job_scraper.py
@@ -1,19 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from .models import Job
+from django.db import IntegrityError
 
 
 def career_beacon_job_scraper(page):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.2 Safari/605.1.15'}
     url = f'https://www.careerbeacon.com/en/search/jobs-in-New-Brunswick?page={page}&jvk=2130348'
     r = requests.get(url, headers)
+   
     soup  = BeautifulSoup(r.content, 'html.parser')
     divs = soup.find_all('div', class_='non_featured_job_container')
+
     for item in divs:
         title = item.find('div', class_='serp_job_title h6 text-primary clickable').text.strip()
         link = item.find('a', class_='serp_job_title d-none')['href']
         company = item.find('span', class_='name fw-semibold text-muted').text.strip()
         location = item.find('span', class_='location text-muted').text.strip()
+
         try:
             date_posted_str = item.find('div', class_='smaller text-muted')['title']
         except:
@@ -24,22 +29,26 @@ def career_beacon_job_scraper(page):
             job_details = ''
         date_posted = datetime.datetime.strptime(date_posted_str, "%Y-%m-%d").date()
 
-        job = {
-            'title': title, 
-            'link': link,
-            'company': company, 
-            'location': location,
-            'date_posted': date_posted,
-            'job_details': job_details
-            }
-        job_list.append(job)
+        try:
+            Job.objects.create(
+            title = title, 
+            link = link,
+            company = company,
+            location = location,
+            date_posted = date_posted, 
+            job_details = job_details
+            )
+        except IntegrityError as e:
+            # Log the error for debugging or potential deduplication strategies
+            print(f"Error creating job: {e}")
 
-    return r.status_code
-## Need to set up the model and start saving this to the that model
+       
+    return 
 
-job_list=[]
-# for i in range(3):
-#     career_beacon_job_scraper(i)
-# print(len(job_list))
+def get_jobs():
+    for i in range(40):
+        career_beacon_job_scraper(i)
+    return
+
 
 
--- JobTracker/JobApi/views.py
+++ JobTracker/JobApi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .job_scraper import career_beacon_job_scraper, get_jobs
+from django.http import HttpResponse
 
 # Create your views here.
+def scrape(request):
+    get_jobs()
+    return HttpResponse("Scraper complete and jobs added to DB.")
+
+
