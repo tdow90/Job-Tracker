13ab53385696aa40cfd5d8589d5a4f6f666bd6a7
Tyler Downing <tyler.ar.downing@hotmail.com>
2025-01-06 20:02:01 -0400

    Added all of the api views

--- JobTracker/JobApi/urls.py
+++ JobTracker/JobApi/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path("scrape/", views.scrape, name="scrape"),
+    path("jobs/", views.JobList.as_view(), name="jobs"),
+    path("new_jobs/", views.NewJobList.as_view(), name="new_jobs"),
+    path("<city>/", views.CityJobList.as_view(), name="city_jobs"),
+    path("new/<city>/", views.NewCityJobList.as_view(), name="new_city_job_list"),
+
 ]
--- JobTracker/JobApi/views.py
+++ JobTracker/JobApi/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .job_scraper import career_beacon_job_scraper, get_jobs
 from django.http import HttpResponse
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser
+from .serializers import JobSerializer 
+from .models import Job
+from django.utils import timezone
+from datetime import timedelta
+
 
 # Create your views here.
 def scrape(request):
@@ -8,3 +15,35 @@ def scrape(request):
     return HttpResponse("Scraper complete and jobs added to DB.")
 
 
+class JobList(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
+
+class NewJobList(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        one_week_ago = timezone.now() - timedelta(days=7)
+        return super().get_queryset().filter(date_posted__gte=one_week_ago)
+    
+class CityJobList(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        city = self.kwargs['city']
+        return super().get_queryset().filter(location__icontains=city)
+    
+class NewCityJobList(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+    permission_classes = [IsAdminUser]
+
+    def get_queryset(self):
+        one_week_ago = timezone.now() - timedelta(days=7)
+        city = self.kwargs['city']
+        return super().get_queryset().filter(date_posted__gte=one_week_ago, location__icontains=city)
