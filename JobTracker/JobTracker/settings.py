391c4a896d85e6818e07a178b6c3080f719f7577
Tyler Downing <tyler.ar.downing@hotmail.com>
2025-02-06 21:57:04 -0400

    trying to fix static file issues when building on Render

--- JobTracker/JobTracker/settings.py
+++ JobTracker/JobTracker/settings.py
@@ -133,7 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+STATIC_URL = ''
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
